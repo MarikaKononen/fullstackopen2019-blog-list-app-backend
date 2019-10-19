b3452f9dfdbda18a9244abd7efeb8fa05d725483
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-10-19 22:24:01 +0300

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  // console.log('blogs from attribute', blogs)
+  let sum = 0
+  // eslint-disable-next-line no-unused-vars
+  const totLikes = blogs.map( blog => sum += blog.likes)
+  // console.log('totalLikesfunction: ', totLikes)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
